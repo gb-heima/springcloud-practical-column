e8013eed74b99ec2b155fb0daed03d49e8a9d2d5
guobin_wb <gbly02@gamil.com>
2019-05-06 12:26:06 +0800

    断路器（Hystrix）ribbon Feign (Finchley版本)

--- sc-f-chapter4/service-feign/src/main/java/com/gblfy/servicefeign/service/SchedualServiceHi.java
+++ sc-f-chapter4/service-feign/src/main/java/com/gblfy/servicefeign/service/SchedualServiceHi.java
@@ -1,15 +1,15 @@
 package com.gblfy.servicefeign.service;
 
+import com.gblfy.servicefeign.service.impl.SchedualServiceHiHystric;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
 
     @GetMapping(value = "/hi")
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
-
 }
