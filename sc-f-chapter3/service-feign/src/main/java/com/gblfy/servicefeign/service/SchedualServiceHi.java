809a36bc3f17f243b2ec469bbc3e342ca5ca751c
guobin_wb <gbly02@gamil.com>
2019-05-11 15:09:52 +0800

    第三篇:服务消费者（Feign）(Finchley版本)V2.0_dev

--- sc-f-chapter3/service-feign/src/main/java/com/gblfy/servicefeign/service/SchedualServiceHi.java
+++ sc-f-chapter3/service-feign/src/main/java/com/gblfy/servicefeign/service/SchedualServiceHi.java
@@ -2,8 +2,6 @@ package com.gblfy.servicefeign.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "service-hi")
